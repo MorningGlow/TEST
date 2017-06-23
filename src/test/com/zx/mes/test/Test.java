88b093011ab041865a8a1bf57473fc8853db5ca6
HYL <931614367@qq.com>
2017-06-23 11:50:31 +0800

    idea 搭建webapp测试，用于代码，框架测试

---
+++ src/test/com/zx/mes/test/Test.java
@@ -0,0 +1,20 @@
+package com.zx.mes.test;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.log4j.Logger;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 2017/6/23.
+ */
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {})
+public class Test {
+    private static final Logger logger=Logger.getLogger(Test.class);
+    @org.junit.Test
+    public void test(){
+        logger.info(JSON.toJSONStringWithDateFormat("测试","yyyy-MM-dd HH:mm:dd"));
+    }
+}
